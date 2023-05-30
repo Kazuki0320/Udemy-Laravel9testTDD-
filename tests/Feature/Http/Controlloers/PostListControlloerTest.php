55478172d8d528839983300eb7fca9a2c5ecadb0
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2023-05-30 20:11:57 +0900

    factory使い方あれこれ

--- tests/Feature/Http/Controlloers/PostListControlloerTest.php
+++ tests/Feature/Http/Controlloers/PostListControlloerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -20,10 +21,32 @@ class PostListControllerTest extends TestCase
         // $post1 = Post::factory()->create();
         // $post2 = Post::factory()->create();
 
-        $this->withoutExceptionHandling();
+        // $this->withoutExceptionHandling();
+        // $this->get('/')
+        //     ->assertOk()
+        //     ->assertSee($post1->title)
+        //     ->assertSee($post2->title);
+
+        $post1 = Post::factory()->create(['title' => 'ブログのタイトル1']);
+        $post2 = Post::factory()->create(['title' => 'ブログのタイトル2']);
+
         $this->get('/')
-            ->assertOk();
-            // ->assertSee($post1->title)
-            // ->assertSee($post2->title);
+            ->assertOk()
+            ->assertSee('ブログのタイトル1')
+            ->assertSee('ブログのタイトル2');
+    }
+
+    /**
+     *
+     * @test
+     */
+    function factoryの観察()
+    {
+        $post = Post::factory()->create();
+        dump($post->toArray());
+
+        dump(User::get()->toArray());
+
+        $this->assertTrue(true);
     }
 }
